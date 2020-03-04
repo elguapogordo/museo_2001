7537228fde24bc7e730a4c870b731d92324db3cf
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-04 10:12:49 -0700

    update setup, add photographs by artist test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -20,6 +20,18 @@ class CuratorTest < Minitest::Test
      artist_id: "2",
      year: "1941"
     })
+    @photo_3 = Photograph.new({
+     id: "3",
+     name: "Identical Twins, Roselle, New Jersey",
+     artist_id: "3",
+     year: "1967"
+    })
+    @photo_4 = Photograph.new({
+     id: "4",
+     name: "Monolith, The Face of Half Dome",
+     artist_id: "3",
+     year: "1927"
+    })
     @artist_1 = Artist.new({
       id: "1",
       name: "Henri Cartier-Bresson",
@@ -27,12 +39,19 @@ class CuratorTest < Minitest::Test
       died: "2004",
       country: "France"
     })
-    artist_2 = Artist.new({
+    @artist_2 = Artist.new({
       id: "2",
       name: "Ansel Adams",
       born: "1902",
       died: "1984",
       country: "United States"
+    })
+    @artist_3 = Artist.new({
+     id: "3",
+     name: "Diane Arbus",
+     born: "1923",
+     died: "1971",
+     country: "United States"
     })
   end
 
@@ -65,8 +84,25 @@ class CuratorTest < Minitest::Test
   def test_it_finds_artist_by_id
     @curator.add_artist(@artist_1)
     @curator.add_artist(@artist_2)
-    
+
     assert_equal @artist_1, @curator.find_artist_by_id("1")
   end
+
+  def test_it_returns_photographs_by_artist
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    expected = ({
+      @artist_1 => [@photo_1],
+      @artist_2 => [@photo_2],
+      @artist_3 => [@photo_3, @photo_4]
+      })
+
+      assert_equal expected, @curator.photographs_by_artist
+    end
 
 end
