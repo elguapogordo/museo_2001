66e7583c529412089974708c97e784219bb2ca54
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-04 09:11:34 -0700

    add class, init with attributes

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
+  end
+
+end
