63e7df41b643f88eee28802e67b9fe9abcf5fcc1
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-04 10:13:08 -0700

    add photographs_by_artist

--- lib/curator.rb
+++ lib/curator.rb
@@ -18,4 +18,15 @@ class Curator
     @artists.find { |artist| artist.id == artist_id }
   end
 
+  def photographs_by_artist
+    photos_by_artist = {}
+    @artists.each do |artist|
+      photos_by_artist[artist] = @photographs.find_all do |photo|
+        photo.artist_id == artist.id
+      end
+    end
+    photos_by_artist
+  end
+
+
 end
