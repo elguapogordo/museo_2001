d41cd343d384876062c50ad5df33d9a0c4cc3e54
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-04 09:38:22 -0700

    add to setup

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -8,6 +8,32 @@ class CuratorTest < Minitest::Test
 
   def setup
     @curator = Curator.new
+    @photo_1 = Photograph.new({
+     id: "1",
+     name: "Rue Mouffetard, Paris (Boy with Bottles)",
+     artist_id: "1",
+     year: "1954"
+    })
+    @photo_2 = Photograph.new({
+     id: "2",
+     name: "Moonrise, Hernandez",
+     artist_id: "2",
+     year: "1941"
+    })
+    @artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
   end
 
   def test_it_exists
@@ -17,5 +43,7 @@ class CuratorTest < Minitest::Test
   def test_it_has_photographs
     assert_equal [], @curator.photographs
   end
+
+
 
 end
