a63f912b8d60ffacc7e145a747da90069385e7be
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-04 09:23:51 -0700

    add init, attributes

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,1 +1,10 @@
+class Photograph
 
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @artist_id = attributes[:artist_id]
+    @year = attributes[:year]
+  end
+
+end
